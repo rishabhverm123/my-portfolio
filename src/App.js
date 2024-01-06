be0169d7ca095e146a2cff3c9c9eb9ca31846e91
Rishabh Verma <41505916+rishabhverm123@users.noreply.github.com>
2024-01-06 12:51:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Rishabh
         </a>
       </header>
     </div>
